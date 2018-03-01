69d4efa7fe8ea0b194aa263c17eac79dd07c0909
Frank <akura11.tt@gmail.com>
2018-03-01 16:21:35 -0600

    Update(qa): Actualizando codigo para corregir vulnerabilidad detectada

--- dgm_tools/dgm_tools/settings.py
+++ dgm_tools/dgm_tools/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME', ''),
         'USER': os.environ.get('DATABASE_USER' , ''),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
-        'HOST': os.environ.get('DATABASE_HOST', '35.188.31.77'),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
         'PORT': os.environ.get('DATABASE_PORT', '5432')
     }
 }
